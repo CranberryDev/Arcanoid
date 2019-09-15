bfec5b6c6fc021853d2a4d76e7aa9e8bd2f4f21a
bootcamp <Agaev.A.R@sberbank.ru>
2019-09-15 12:33:23 +0300

    add 'choseLevel' button to main screen rewrite method of creating buttons

--- Arcanoid/interface/MenuCreator.swift
+++ Arcanoid/interface/MenuCreator.swift
@@ -56,45 +56,47 @@ class MenuCreator {
         scene.addChild(gameTitle)
         
         //Init play button
-        createPlayButton()
+        createButton(text: "Play", name: "playButtonNode", mainOffset: 0, obj: &playButton)
+        createButton(text: "Level", name: "choseLevelNode", mainOffset: 150, obj: &levelButton)
         showMenu()
         
     }
     
     
-    private func createPlayButton() {
+    private func createButton(text: String, name: String, mainOffset: Int, obj: inout ButtonComponents) {
         let width = 250
         let height = 100
         let cornerRadius:CGFloat = CGFloat(40)
         let outerX = width / -2
         let outerY = Int(scene.size.height / 5)
         
-        let outerRect: CGRect = CGRect(x: outerX, y: outerY, width: width, height: height)
-        playButton.outerShape = SKShapeNode(rect: outerRect, cornerRadius: cornerRadius + 10)
-        playButton.outerShape.zPosition = 1
-        playButton.outerShape.fillColor = SKColor.black
+        let outerRect: CGRect = CGRect(x: outerX, y: outerY - mainOffset, width: width, height: height)
+        obj.outerShape = SKShapeNode(rect: outerRect, cornerRadius: cornerRadius + 10)
+        obj.outerShape.zPosition = 1
+        obj.outerShape.fillColor = SKColor.black
+        obj.outerShape.name = name
         
         
         let xOffset = 7
         let yOffset = 7
         let innerWidth = width - (xOffset * 2)
         let innerHeight = height - (yOffset * 2)
-        let innerRect: CGRect = CGRect(x: outerX + xOffset, y: outerY + yOffset, width: innerWidth, height: innerHeight)
-        playButton.innerShape = SKShapeNode(rect: innerRect, cornerRadius: cornerRadius)
-        playButton.innerShape.fillColor = SKColor.white
-        playButton.innerShape.zPosition = 1
+        let innerRect: CGRect = CGRect(x: outerX + xOffset, y: outerY + yOffset - mainOffset, width: innerWidth, height: innerHeight)
+        obj.innerShape = SKShapeNode(rect: innerRect, cornerRadius: cornerRadius)
+        obj.innerShape.fillColor = SKColor.white
+        obj.innerShape.zPosition = 1
         
-        playButton.textLabel = SKLabelNode(fontNamed: fontName)
-        playButton.textLabel.text = "Play"
-        playButton.textLabel.fontSize = 50
-        playButton.textLabel.fontColor = SKColor.black
-        playButton.textLabel.zPosition = 1
-        playButton.textLabel.position = CGPoint(x: 0, y: outerY + innerHeight/3 + yOffset)
-        playButton.textLabel.isHidden = false
+        obj.textLabel = SKLabelNode(fontNamed: fontName)
+        obj.textLabel.text = text
+        obj.textLabel.fontSize = 50
+        obj.textLabel.fontColor = SKColor.black
+        obj.textLabel.zPosition = 1
+        obj.textLabel.position = CGPoint(x: 0, y: outerY + innerHeight/3 + yOffset - mainOffset)
+        obj.textLabel.isHidden = false
         
-        scene.addChild(playButton.outerShape)
-        scene.addChild(playButton.innerShape)
-        scene.addChild(playButton.textLabel)
+        scene.addChild(obj.outerShape)
+        scene.addChild(obj.innerShape)
+        scene.addChild(obj.textLabel)
     }
     
 }
